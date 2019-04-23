0b14bd3281a69b45bd3560a0ab1811775f127197
Nikolina <nn.lion.land@gmail.com>
2019-04-23 12:09:46 +0200

    map

--- js/main.js
+++ js/main.js
@@ -134,3 +134,14 @@ $('.modal .close').on('click', function(event) {
 //     $('#map').on('click',initialize)
 // });
 /*********** Map ***********/
+
+function mapsSelector() {
+  if /* if we're on iOS, open in Apple Maps */
+    ((navigator.platform.indexOf("iPhone") != -1) || 
+     (navigator.platform.indexOf("iPod") != -1) || 
+     (navigator.platform.indexOf("iPad") != -1))
+    window.open("maps://maps.google.com/maps?daddr=doe,meadow&amp;ll=");
+
+  else /* else use Google */
+    window.open("https://maps.google.com/maps?daddr=doe,meadow&amp;ll=");
+}
